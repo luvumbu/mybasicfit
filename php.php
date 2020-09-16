5ff75c2ee4ff51ea31c3da1a9f6b4545ac75875e
Luvumbu <luvumbu.n@gmail.com>
2020-09-16 11:04:45 +0200

    add foders

--- php.php
+++ php.php
@@ -1,6 +1,45 @@
 <?php
 
 session_start();
+ 
+ 
+$REMOTE_ADDR =   $_SERVER['REMOTE_ADDR'];
+if ($REMOTE_ADDR == "localhost" || $REMOTE_ADDR == "127.0.0.1" || $REMOTE_ADDR=="::1" ) {
+ 
+	$localisation = "localhost"; // atribution delement variable	
+	$_SESSION["REMOTE_ADDR"] = $localisation;
+	$_SESSION["country"] = "country_local";
+  $_SESSION["city"] = "city_local";
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+  $dbname = "basicfit"; 
+} else {
+  $servername = "localhost";
+  $username = "u481158665_basicfit";
+  $password = "v3p9r3e@59A";
+  $dbname = $username;
+
+}
+$_SESSION["localisation"] = $localisation;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  $_SESSION["nom"] =sha1(rand(5, 1000));
 
  $nom=$_SESSION["nom"];
@@ -27,4 +66,3 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
-?>
