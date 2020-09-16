9b0ebaa94b220a19fd1d44df700747a2445aaa45
Luvumbu <luvumbu.n@gmail.com>
2020-09-16 11:08:28 +0200

    update

--- php.php
+++ php.php
@@ -4,23 +4,12 @@ session_start();
  
  
 $REMOTE_ADDR =   $_SERVER['REMOTE_ADDR'];
-if ($REMOTE_ADDR == "localhost" || $REMOTE_ADDR == "127.0.0.1" || $REMOTE_ADDR=="::1" ) {
- 
-	$localisation = "localhost"; // atribution delement variable	
-	$_SESSION["REMOTE_ADDR"] = $localisation;
-	$_SESSION["country"] = "country_local";
-  $_SESSION["city"] = "city_local";
-  $servername = "localhost";
-  $username = "root";
-  $password = "root";
-  $dbname = "basicfit"; 
-} else {
-  $servername = "localhost";
-  $username = "u481158665_basicfit";
-  $password = "v3p9r3e@59A";
-  $dbname = $username;
 
-}
+$servername = "localhost";
+$username = "u481158665_basicfit";
+$password = "v3p9r3e@59A";
+$dbname = $username;
+
 $_SESSION["localisation"] = $localisation;
 
 
